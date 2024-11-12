d1050c9730baeb851f82d7daffae745cd058d387
awesomejaveed <javeedshaik1408@gmail.com>
2024-11-12 20:21:18 +0530

    all files

--- src/NOV/EX081124/LAB001.java
+++ src/NOV/EX081124/LAB001.java
@@ -1,4 +1,8 @@
 package NOV.EX081124;
 
 public class LAB001 {
+    public static void main(String[] args) {
+        System.out.println("jmunjm");
+        System.out.println("unjm");
+    }
 }
---
+++ src/NOV/EX121124/LAB0002.java
@@ -0,0 +1,7 @@
+package NOV.EX121124;
+
+public class LAB0002 {
+    public static void main(String[] args) {
+        System.out.println("the point where at which ");
+    }
+}
