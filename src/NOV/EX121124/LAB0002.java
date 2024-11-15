360907bcf9cb69e14264c59331ded72b47759abb
awesomejaveed <javeedshaik1408@gmail.com>
2024-11-16 02:43:25 +0530

    all files

--- src/NOV/EX121124/LAB0002.java
+++ src/NOV/EX121124/LAB0002.java
@@ -2,6 +2,23 @@ package NOV.EX121124;
 
 public class LAB0002 {
     public static void main(String[] args) {
-        System.out.println("the point where at which ");
+
+        int name = 50;
+        System.out.printf("my name is %d",name);
+        System.out.println();
+        int num = 15;
+        System.out.printf("%d x %d = %d",num,1,num*1);
+        System.out.println();
+        System.out.printf("%d x %d = %d",num,2,num*2);
+        System.out.println();
+        System.out.printf("%d x %d = %d",num,3,num*3);
+        System.out.println();
+        System.out.printf("%d x %d = %d",num,4,num*4);
+        System.out.println();
+        System.out.printf("%d x %d = %d",num,5,num*5);
+        System.out.println();
+        System.out.printf("%d x %d = %d",num,6,num*6);
+
+
     }
 }
---
+++ src/NOV/EX121124/LAB0003.java
@@ -0,0 +1,17 @@
+package NOV.EX121124;
+
+public class LAB0003 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println!(a&&b);
+        System.out.println!(a||b);
+//        System.out.println(a>=b);
+//        System.out.println(a<=b);
+
+
+
+
+
+    }
+}
