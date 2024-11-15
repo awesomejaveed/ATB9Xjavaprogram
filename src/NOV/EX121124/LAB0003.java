1bb7b20e4bd317fd7d40e242445a456722b06431
awesomejaveed <javeedshaik1408@gmail.com>
2024-11-16 02:47:20 +0530

    all files

--- src/NOV/EX121124/LAB0003.java
+++ src/NOV/EX121124/LAB0003.java
@@ -4,8 +4,8 @@ public class LAB0003 {
     public static void main(String[] args) {
         boolean a = true;
         boolean b = false;
-        System.out.println!(a&&b);
-        System.out.println!(a||b);
+        System.out.println(a&&b);
+        System.out.println(a||b);
 //        System.out.println(a>=b);
 //        System.out.println(a<=b);
 
